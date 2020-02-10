4b0960d91b0dbb54248738bdf9db21305046834f
AnaVi <secret>
2020-02-10 10:18:55 +0100

    fini les execices js 05 opérateurs

---
+++ js-5-operateurs/script.js
@@ -0,0 +1,13 @@
+var a = "100";
+var b = 100;
+var c = 1.00;
+var d = true;
+var i;
+
+console.log("Ceci est une chaîne de charactères : " + a);
+for (i = 0; i < 10; i++) {
+    console.log("The value of b is " + b);
+    b--
+}
+console.log(c + Number(a));
+console.log(d = false);
