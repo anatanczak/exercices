f00dbf8bcc8131c676dbd3f85f2ad646222358b9
AnaVi <secret>
2020-02-10 16:28:25 +0100

    fini ex 3 calculette

--- js-6-conditionnee/ex-3/script.js
+++ js-6-conditionnee/ex-3/script.js
@@ -1,36 +1,78 @@
-let userInput;
-let inputIsValid = false;
-let numberToTest;
+let userInputFirstNumber;
+let userInputSecondNumber;
+let userInputForOperator;
+let operatorIsValid = false;
+let firstInt;
+let secondInt;
 
 
-userInput = prompt("Entrez un nombre entier ?");
+userInputFirstNumber = prompt("Entrez un nombre en entier.");
+firstInt = parseInt(userInputFirstNumber);
+
+while (isNaN(firstInt)) {
+    userInputFirstNumber = prompt("Vous vous êtes trompé. Entrez un nombre en entier.");
+    firstInt = parseInt(userInputFirstNumber);
+}
+
+userInputSecondNumber = prompt("Entrez un autre nombre en entier.");
+secondInt = parseInt(userInputSecondNumber)
 
 
-// check if user input can be converted into an int
-function checkIfStringIsValid(theInput) {
-
-    if (theInput == null || theInput == undefined) {
-        return false
-    } else if (theInput === "") {
-        return false
-    } else {
-        return !isNaN(theInput);
-    }
-
+while (isNaN(secondInt)) {
+    userInputSecondNumber = prompt("Vous vous êtes trompé. Entrez un nombre en entier.");
+    secondInt = parseInt(userInputSecondNumber);
 }
 
 
-inputIsValid = checkIfStringIsValid(userInput)
 
-// //convert user input into an int
 
-function convertStringIntoInt(theInput, valid) {
-    if (valid) {
-        let integer = parseInt(theInput);
-        return integer
-    } else {
-        return 0;
+
+while (operatorIsValid == false) {
+
+    userInputForOperator = prompt("Entrez un seul operateur : +, -, * ou /");
+    console.log(userInputForOperator)
+    console.log(operatorIsValid)
+    switch (userInputForOperator) {
+        case "+":
+            operatorIsValid = true;
+            break;
+        case "-":
+            operatorIsValid = true;
+            break;
+        case "*":
+            operatorIsValid = true;
+            break;
+        case "/":
+            operatorIsValid = true;
+            break;
+        default:
+            operatorIsValid = false;
+            break;
     }
+
+    if (!operatorIsValid) {
+        alert("Vous vous êtes trompé.");
+    } else {
+        alert("Vous avez entré un operateur valide.");
+    }
+    console.log(operatorIsValid)
 }
 
-numberToTest = convertStringIntoInt(userInput, inputIsValid);
+if (secondInt === 0 && userInputForOperator === "/") {
+    alert("Désolé, sur cette planète la division par 0 n'est pas possible.")
+} else {
+    switch (userInputForOperator) {
+        case "+":
+            alert(firstInt + secondInt)
+            break;
+        case "-":
+            alert(firstInt - secondInt)
+            break;
+        case "*":
+            alert(firstInt * secondInt)
+            break;
+        case "/":
+            alert(firstInt / secondInt)
+            break;
+    }
+}
