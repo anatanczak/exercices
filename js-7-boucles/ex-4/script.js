f88cfe9b762e27cb9e44ec07fa50193b25af6983
AnaVi <secret>
2020-02-11 16:55:11 +0100

    almost finishe ex4

---
+++ js-7-boucles/ex-4/script.js
@@ -0,0 +1,25 @@
+let numbers = [];
+let answer;
+let nameNumber = 1;
+let enteredNumber;
+let sum = 0;
+let average;
+
+// je ne traite pas toute les erreures possibles lors de la saisie
+do {
+    answer = prompt("saisissez un entier Nr°" + nameNumber + "ou entrer le 0 pour arrêter le saisie.");
+    nameNumber++;
+    enteredNumber = parseInt(answer)
+    if (!isNaN(enteredNumber)) {
+        numbers.push(enteredNumber);
+    }
+} while (answer !== "0");
+
+let lastElement = numbers.pop()
+
+for (const item of numbers) {
+    sum = sum + item;
+}
+average = sum / numbers.length
+
+// TO DO show the sum and the average
