3c60e0071fb20b9132ea0958b8c55eaa3821d403
AnaVi <secret>
2020-02-11 09:57:12 +0100

    ex 4 fini

---
+++ js-6-conditionnee/ex-4/script.js
@@ -0,0 +1,45 @@
+let single;
+let numberOfKidsString;
+let numberOfKidsInt = 0;
+let lowWage;
+let bossPart;
+
+
+single = confirm("Etes-vous celibataire ?")
+
+numberOfKidsString = prompt("Combien d'enfant avez-vous ? Entrer 0 si vous n'en avez pas.");
+numberOfKidsInt = parseInt(numberOfKidsString);
+
+while (isNaN(numberOfKidsInt)) {
+    numberOfKidsString = prompt("Vous vous êtes trompé. Entrez un nombre en entier.");
+    numberOfKidsInt = parseInt(numberOfKidsString);
+}
+
+lowWage = confirm("Est votre salaire mesuel inférieur à 1200 € ?")
+
+
+if (single && lowWage) {
+    bossPart = 30;
+} else if (single && !lowWage) {
+    bossPart = 20;
+} else if (!single && lowWage) {
+    bossPart = 35;
+} else {
+    bossPart = 25;
+}
+
+if (numberOfKidsInt > 0) {
+    let supp = numberOfKidsInt * 10;
+    let result = bossPart + supp;
+
+    if (result >= 50) {
+        bossPart = 50;
+    } else {
+        bossPart = result;
+    }
+
+
+}
+
+
+alert("La participation du patron est " + bossPart + "%");
