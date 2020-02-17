c532d0f7a6ea0e742344acb149e3f8a40f9b188e
AnaVi <secret>
2020-02-17 09:07:05 +0100

    fini ex 3

--- js-10-arrays/ex-3/script.js
+++ js-10-arrays/ex-3/script.js
@@ -1,26 +1,48 @@
 //TO DO : Not working;
-function bubbleSort(unsortedArray) {
-  let swap;
 
-  do {
-    for (i = 0; i < unsortedArray.length; i++) {
-      if (unsortedArray[i] > unsortedArray[i + 1]) {
-        let temp = unsortedArray[i];
-        unsortedArray[i] = unsortedArray[i + 1];
-        unsortedArray[i + 1] = temp;
-        swap = true;
-      } else {
-        swap = false;
+function bubbleSort(unsortedArray) {
+  for (i = 0; i < unsortedArray.length; i++) {
+    for (j = 0; j < unsortedArray.length - 1; j++) {
+      if (unsortedArray[j] > unsortedArray[j + 1]) {
+        let temp = unsortedArray[j];
+        unsortedArray[j] = unsortedArray[j + 1];
+        unsortedArray[j + 1] = temp;
       }
     }
-  } while (swap === true)
+  }
+  console.log("sorted:" + unsortedArray)
 }
 
-// let array1 = [1, 2, 3, 4, 5];
 let array2 = [1, 9, 5, 7, 1, 0, 5];
 
-// bubbleSort(array1);
 bubbleSort(array2);
-
-// console.log(array1);
 console.log(array2);
+
+
+
+
+// function bubbleSort(unsortedArray) {
+//   let swap;
+
+//   do {
+//     for (i = 0; i < unsortedArray.length; i++) {
+//       if (unsortedArray[i] > unsortedArray[i + 1]) {
+//         let temp = unsortedArray[i];
+//         unsortedArray[i] = unsortedArray[i + 1];
+//         unsortedArray[i + 1] = temp;
+//         swap = true;
+//       } else {
+//         swap = false;
+//       }
+//     }
+//   } while (swap === true)
+// }
+
+// // let array1 = [1, 2, 3, 4, 5];
+// let array2 = [1, 9, 5, 7, 1, 0, 5];
+
+// // bubbleSort(array1);
+// bubbleSort(array2);
+
+// // console.log(array1);
+// console.log(array2);
