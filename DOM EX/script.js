74bce561d357477aa75f4ece63d05898492ff0d6
AnaVi <secret>
2020-02-17 12:50:30 +0100

    fini ex 2 the magic number

--- DOM EX/script.js
+++ DOM EX/script.js
@@ -1,12 +1,51 @@
-document.getElementById("myButton").addEventListener("click", popupFunc);
+// // Ex 1
+
+// document.getElementById("myButton").addEventListener("click", popupFunc);
 
 
-function popupFunc() {
+// function popupFunc() {
 
+//   let userInput = document.getElementById("textInput").value;
+
+//   if (userInput === "") {
+//     userInput = "Rien du tout"
+//   }
+//   alert("Vous avez saisi: '" + userInput + "'");
+// }
+
+
+// ex 2 Nombre Magic
+let randomNumber = Math.ceil(Math.random() * 100);
+
+
+document.getElementById("task").innerHTML = " Devinez le nombre magic (entre 0 et 100 ) <br> Entrez votre proposition: "
+
+document.getElementById("myButton").innerHTML = "Verifier"
+
+document.getElementById("myButton").addEventListener("click", verif);
+
+function verif() {
   let userInput = document.getElementById("textInput").value;
 
-  if (userInput === "") {
-    userInput = "Rien du tout"
+  let number = parseInt(userInput);
+  let textToDisplay = "Vous n'avez pas rentré un numero valable"
+  console.log(randomNumber);
+
+  if (number === randomNumber) {
+    textToDisplay = "Bravo!"
+    setTimeout(() => {
+      if (!window.location.hash) {
+        window.location = window.location + '#loaded';
+        window.location.reload();
+      }
+    }, 3000);
+
+  } else if (number < randomNumber) {
+    textToDisplay = "Plus grand!"
+  } else if (number > randomNumber) {
+    textToDisplay = "Plus petit!"
   }
-  alert("Vous avez saisi: '" + userInput + "'");
+
+  document.getElementById("task").innerHTML = textToDisplay;
+
 }
